f3f4dd7442731cb96d4f1ef56f270b581e1023c4
xstefank <xstefank122@gmail.com>
2018-07-06 19:24:41 +0200

    Add static env loading to wildfly-env-var

--- wildfly-env-var/src/main/java/org/xstefank/EnvEndpoint.java
+++ wildfly-env-var/src/main/java/org/xstefank/EnvEndpoint.java
@@ -10,13 +10,21 @@ public class EnvEndpoint {
 
     private static final String envName = "TEST_ENV";
     private static final Logger log = Logger.getLogger(EnvEndpoint.class);
-    
+
+    private final String testStaticEnv = readStaticEnv();
+
     @GET
     @Path("/get")
     public String getEnv() {
         log.info("System.getenv() " + System.getenv(envName));
         log.info("System.getProperty() " + System.getProperty(envName));
 
+        log.info("TEST_STATIC_ENV=" + testStaticEnv);
+
         return System.getenv(envName);
+    }
+
+    private String readStaticEnv() {
+        return System.getenv("TEST_STATIC_ENV");
     }
 }
